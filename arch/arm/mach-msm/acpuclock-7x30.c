c55d3cec62d0b36d8544b83609eac3985cf596ca
Bylos <bylos72@gmail.com>
2012-04-30 12:01:31 +0100

    Enable overclock : - Add new frequencies up to 1.7 GHz - Remove PLL2 limitation - Set cpu clock to 1 Ghz at boot

--- arch/arm/mach-msm/acpuclock-7x30.c
+++ arch/arm/mach-msm/acpuclock-7x30.c
@@ -94,10 +94,16 @@ static struct clock_state drv_state = { 0 };
 static struct clkctl_acpu_speed *backup_s;
 
 static struct pll pll2_tbl[] = {
-	{  42, 0, 1, 0 }, /*  806 MHz */
-	{  53, 1, 3, 0 }, /* 1024 MHz */
-	{ 125, 0, 1, 1 }, /* 1200 MHz */
-	{  73, 0, 1, 0 }, /* 1401 MHz */
+	{42, 0, 1, 0},		/* 806 MHz */
+	{48, 1, 3, 0},		/* 921 MHz */
+	{53, 1, 3, 0},		/* 1024 MHz */
+	{58, 1, 3, 0},		/* 1113 MHz */
+	{63, 1, 3, 0},		/* 1200 MHz */
+	{68, 1, 3, 0},		/* 1305 MHz */
+	{73, 1, 3, 0},		/* 1401 MHz */
+	{78, 1, 3, 0},		/* 1516 MHz */
+	{83, 1, 3, 0},		/* 1612 MHz */
+	{88, 1, 3, 0},		/* 1708 MHz */
 };
 
 /* Use negative numbers for sources that can't be enabled/disabled */
@@ -110,23 +116,27 @@ static struct pll pll2_tbl[] = {
  * know all the h/w requirements.
  */
 static struct clkctl_acpu_speed acpu_freq_tbl[] = {
-	{ 0, 24576,  SRC_LPXO, 0, 0,  30720000,  900, VDD_RAW(900) },
-	{ 0, 61440,  PLL_3,    5, 11, 61440000,  900, VDD_RAW(900) },
-	{ 1, 122880, PLL_3,    5, 5,  61440000,  900, VDD_RAW(900) },
-	{ 0, 184320, PLL_3,    5, 4,  61440000,  900, VDD_RAW(900) },
-	{ 0, MAX_AXI_KHZ, SRC_AXI, 1, 0, 61440000, 900, VDD_RAW(900) },
-	{ 1, 245760, PLL_3,    5, 2,  61440000,  900, VDD_RAW(900) },
-	{ 1, 368640, PLL_3,    5, 1,  122800000, 900, VDD_RAW(900) },
-	/* AXI has MSMC1 implications. See above. */
-	{ 1, 768000, PLL_1,    2, 0,  153600000, 1050, VDD_RAW(1050) },
-	/*
-	 * AXI has MSMC1 implications. See above.
-	 */
-	{ 1, 806400,  PLL_2, 3, 0, UINT_MAX, 1100, VDD_RAW(1100), &pll2_tbl[0]},
-	{ 1, 1024000, PLL_2, 3, 0, UINT_MAX, 1200, VDD_RAW(1200), &pll2_tbl[1]},
-	{ 1, 1200000, PLL_2, 3, 0, UINT_MAX, 1200, VDD_RAW(1200), &pll2_tbl[2]},
-	{ 1, 1401600, PLL_2, 3, 0, UINT_MAX, 1250, VDD_RAW(1250), &pll2_tbl[3]},
-	{ 0 }
+	{0,   24576, SRC_LPXO, 0,  0,  30720000,  900, VDD_RAW(900)},
+	{0,   61440,    PLL_3, 5, 11,  61440000,  900, VDD_RAW(900)},
+	{1,  122880,    PLL_3, 5,  5,  61440000,  900, VDD_RAW(900)},
+	{1,  184320,    PLL_3, 5,  4,  61440000,  900, VDD_RAW(900)},
+	{1,  245760,    PLL_3, 5,  2,  61440000,  900, VDD_RAW(900)},
+	{0,  368640,    PLL_3, 5,  1, 122800000,  900, VDD_RAW(900)},
+	{1,  460800,    PLL_1, 2,  0, 153600000,  950, VDD_RAW(950)},
+	{0,  576000,    PLL_1, 2,  0, 153600000, 1000, VDD_RAW(1000)},
+	{0,  652800,    PLL_1, 2,  0, 153600000, 1050, VDD_RAW(1050)},
+	{1,  768000,    PLL_1, 2,  0, 153600000, 1050, VDD_RAW(1050)},
+	{1,  806400,    PLL_2, 3,  0,  UINT_MAX, 1100, VDD_RAW(1100), &pll2_tbl[0]},
+	{1,  921600,    PLL_2, 3,  0,  UINT_MAX, 1150, VDD_RAW(1150), &pll2_tbl[1]},
+	{1, 1024000,    PLL_2, 3,  0,  UINT_MAX, 1200, VDD_RAW(1200), &pll2_tbl[2]},
+	{1, 1113000,    PLL_2, 3,  0,  UINT_MAX, 1200, VDD_RAW(1200), &pll2_tbl[3]},
+	{1, 1200000,    PLL_2, 3,  0,  UINT_MAX, 1200, VDD_RAW(1200), &pll2_tbl[4]},
+	{1, 1305600,    PLL_2, 3,  0,  UINT_MAX, 1250, VDD_RAW(1250), &pll2_tbl[5]},
+	{1, 1401600,    PLL_2, 3,  0,  UINT_MAX, 1250, VDD_RAW(1250), &pll2_tbl[6]},
+	{1, 1516800,    PLL_2, 3,  0,  UINT_MAX, 1300, VDD_RAW(1300), &pll2_tbl[7]},
+	{1, 1612800,    PLL_2, 3,  0,  UINT_MAX, 1350, VDD_RAW(1350), &pll2_tbl[8]},
+	{1, 1708800,    PLL_2, 3,  0,  UINT_MAX, 1400, VDD_RAW(1400), &pll2_tbl[9]},
+	{0}
 };
 
 #define POWER_COLLAPSE_KHZ MAX_AXI_KHZ
@@ -453,20 +463,11 @@ static inline void setup_cpufreq_table(void) { }
 void __init pll2_fixup(void)
 {
 	struct clkctl_acpu_speed *speed = acpu_freq_tbl;
-	u8 pll2_l = readl(PLL2_L_VAL_ADDR) & 0xFF;
 
 	for ( ; speed->acpu_clk_khz; speed++) {
 		if (speed->src != PLL_2)
 			backup_s = speed;
-		if (speed->pll_rate && speed->pll_rate->l == pll2_l) {
-			speed++;
-			speed->acpu_clk_khz = 0;
-			return;
-		}
 	}
-
-	pr_err("Unknown PLL2 lval %d\n", pll2_l);
-	BUG();
 }
 
 #define RPM_BYPASS_MASK	(1 << 3)
@@ -486,9 +487,9 @@ void __init msm_acpu_clock_init(struct msm_acpu_clock_platform_data *clkdata)
 	lpj_init();
 	setup_cpufreq_table();
 
-	for (s = acpu_freq_tbl; s->acpu_clk_khz != 0; s++)
+	/* set cpu clock at 1Ghz for boot */
+	for (s = acpu_freq_tbl; s->acpu_clk_khz != 1024000; s++)
 		;
-	s--;
 	acpuclk_set_rate(0, s->acpu_clk_khz, SETRATE_CPUFREQ);
 	pr_info("ACPU init done, clock rate now : %d\n",
 			drv_state.current_speed->acpu_clk_khz);
